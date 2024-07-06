beb7116ad488f829d14497fdd3da2fd208590dd7
Amazing Dr. Marz <drmarz@manonthemoonstudio.com>
2024-07-05 17:42:49 -0700

    Update calibration with some changes

--- motor_calibration/motor_calibration.ino
+++ motor_calibration/motor_calibration.ino
@@ -2,24 +2,38 @@
 int motorDrivePin = 2;
 int motorSensorPin = 11;
 
-long motorPulseLength = 10;
-long motorPulseDelay = 100;
+int ledMotor = 4;
+int ledSensor = 3;
+
+long motorPulseLength = 60;
+long motorPulseDelay = 120;
 long pulseStart = 0;
 
 
 void setup() {
-  pinMode(motorSensorPin, INPUT);
+  pinMode(motorSensorPin, INPUT_PULLUP);
   pinMode(motorDrivePin, OUTPUT);
+  pinMode(ledMotor, OUTPUT);
+  pinMode(ledSensor, OUTPUT);
 }
 
 void loop() {
+  //int sensorValue = digitalRead(motorSensorPin);
+  // int motorValue = digitalRead(motorDrivePin);
+  // Serial.println(sensorValue + ", "  + motorValue);
+  // delay(1);
   long now = millis();
+
+  digitalWrite(ledSensor, digitalRead(motorSensorPin));
   if (now < pulseStart + motorPulseLength) {
     digitalWrite(motorDrivePin, true);
+    digitalWrite(ledMotor, false);
   } else {
     digitalWrite(motorDrivePin, false);
+    digitalWrite(ledMotor, true);
   }
   if (now > pulseStart + motorPulseDelay) {
     pulseStart = now;
   }
+
 }
